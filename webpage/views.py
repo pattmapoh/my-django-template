8728386fd1836117b3166e72da0bfc92d9280c73
vasinee04 <mr.unging2546@gmail.com>
2025-06-30 09:59:11 +0700

    add page to app

--- webpage/views.py
+++ webpage/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def index (request):
+    return HttpResponse("Hello, World! This is the index page.")
+
+
+def index (request):
+    return HttpResponse("This is the index page.")
+
+
+def index (request):
+    return HttpResponse("This is the index page.")
